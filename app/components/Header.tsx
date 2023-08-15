696e5155a11c32580569c3f6fc4a63d7a7dc8dff
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-08-15 22:01:14 +0400

    adding link to the header logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import Navigation from "./Navigation";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex justify-between px-16 py-6">
-      <Image src="/vectors/nqLogo.svg" width={41} height={29} alt="logo" />
+      <Link href="/">
+        <Image src="/vectors/nqLogo.svg" width={41} height={29} alt="logo" />
+      </Link>
       <Navigation />
       <select
         name="language-selector"

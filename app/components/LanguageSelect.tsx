4ca9848b893fed396f8ca6c49d03e911751f08cf
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-08-16 14:14:38 +0400

    language select component animations

--- app/components/LanguageSelect.tsx
+++ app/components/LanguageSelect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect, useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { getColorRGB } from "../utils/tailwind";
 
 const languages = {
@@ -14,11 +14,28 @@ const MotionImage = motion(Image);
 const LanguageSelect = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [currentLang, setCurrentLang] = useState<keyof typeof languages>("eng");
+  const selectorRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    function clickOutside(e: MouseEvent) {
+      const selector = selectorRef.current;
+      if (!selector || !e.target) return;
+      if (selector.contains(e.target as Node)) return;
+
+      setIsOpen(false);
+    }
+
+    window.addEventListener("click", clickOutside);
+
+    return () => {
+      window.removeEventListener("click", clickOutside);
+    };
+  }, []);
 
   return (
     <div className="relative">
       <motion.button
-        id="language-selector"
+        ref={selectorRef}
         className="ml-auto flex w-fit items-center bg-black  text-slate-400"
         whileHover={{ color: getColorRGB("slate-300") }}
         onClick={() => {
@@ -42,36 +59,40 @@ const LanguageSelect = () => {
           animate={{ rotateZ: isOpen ? "180deg" : "0deg" }}
         />
       </motion.button>
-      {isOpen && (
-        <motion.ul
-          className="absolute right-0 top-[100%] mt-4 flex flex-col rounded-lg bg-neutral-900 text-slate-300"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
-          {Object.entries(languages).map(([key, language]) => (
-            <li key={key}>
-              <motion.button
-                className="flex w-full items-center gap-3 rounded-lg px-3 py-[6px]"
-                whileHover={{
-                  backgroundColor: getColorRGB("neutral-800"),
-                }}
-                onClick={() => {
-                  setCurrentLang(key as keyof typeof languages);
-                }}
-              >
-                <Image
-                  src={language.image}
-                  width={16}
-                  height={16}
-                  alt="english flag"
-                  className="h-4 w-4"
-                />
-                {language.label}
-              </motion.button>
-            </li>
-          ))}
-        </motion.ul>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            className="absolute right-0 top-[100%] mt-4 flex flex-col rounded-lg bg-neutral-900 text-slate-300"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {Object.entries(languages).map(([key, language]) => (
+              <li key={key}>
+                <motion.button
+                  className="flex w-full items-center gap-3 rounded-lg px-3 py-[6px]"
+                  whileHover={{
+                    backgroundColor: getColorRGB("neutral-800"),
+                    color: getColorRGB("slate-200")
+                  }}
+                  onClick={() => {
+                    setCurrentLang(key as keyof typeof languages);
+                  }}
+                >
+                  <Image
+                    src={language.image}
+                    width={16}
+                    height={16}
+                    alt="english flag"
+                    className="h-4 w-4"
+                  />
+                  {language.label}
+                </motion.button>
+              </li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </div>
   );
 };

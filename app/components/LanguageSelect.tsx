9954369a1471ceb807a70b453e552fb3419c6513
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-08-16 12:31:29 +0400

    language select component formatting

--- app/components/LanguageSelect.tsx
+++ app/components/LanguageSelect.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const LanguageSelect = () => {
   return (
-      <select
-        name="language-selector"
-        id="language-selector"
-        className="ml-auto w-fit bg-black text-slate-300"
-      >
-        <option value="eng">English</option>
-        <option value="geo">ქართული</option>
-      </select>
-  )
-}
+    <select
+      name="language-selector"
+      id="language-selector"
+      className="ml-auto w-fit bg-black text-slate-300"
+    >
+      <option value="eng">English</option>
+      <option value="geo">ქართული</option>
+    </select>
+  );
+};
 
-export default LanguageSelect
+export default LanguageSelect;

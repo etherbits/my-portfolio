f990d7f7272b323453c3efa6170c2cef72a20bd3
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-08-27 14:49:00 +0400

    mobile journey

--- app/components/JourneyPageContent.tsx
+++ app/components/JourneyPageContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import OutlinedText from "@/app/components/OutlineText";
-import { cn } from "@/app/utils/tailwind";
+import { cn, getColorRGBA } from "@/app/utils/tailwind";
 import Button from "@/app/components/Button";
 import { generateTranslator } from "../utils/i18n";
 import { DictionarySection } from "../[lang]/dictionaries";
@@ -22,32 +22,36 @@ const JourneyPageContent: React.FC<Props> = ({ journeyDict }) => {
   return (
     <>
       <OutlinedText>Journey</OutlinedText>
-      <main className="w-clamp-card-container">
+      <main className="w-full md:w-clamp-card-container">
         <ul className="mt-[3vh] grid w-[100%] items-center [grid-auto-rows:1fr]">
           {journeys.map((journey, i) => {
             const journeyCardDict = t(journey.id);
             return (
               <li
                 key={journey.id}
-                className="relative z-[-1] flex h-full flex-col justify-center p-4"
+                className="relative z-[-1] flex h-full flex-col justify-center py-4"
               >
                 <section
                   className={cn(
-                    "my-auto flex h-fit w-[46%] flex-col rounded-xl bg-gradient-to-r from-neutral-950 to-neutral-900 px-6 py-4",
+                    "my-auto flex h-fit w-full flex-col rounded-xl bg-gradient-to-r from-neutral-950 to-neutral-900 px-6 py-4 md:w-[46%]",
                     {
-                      "ml-auto": i % 2 !== 0,
-                      "from-neutral-900 to-neutral-950": i % 2 !== 0,
+                      "md:ml-auto": i % 2 !== 0,
+                      "md:from-neutral-900 md:to-neutral-950": i % 2 !== 0,
                     },
                   )}
                 >
-                  <div className="flex justify-between">
+                  <div className="mb-2 flex items-center justify-between gap-2">
                     <h3 className="text-clamp-xl">{journeyCardDict.title}</h3>
                     <span className="text-clamp-sm">{journey.date}</span>
                   </div>
                   <p className="text-clamp-rg">{journeyCardDict.body}</p>
                 </section>
-                <div className="absolute left-[50%] top-[50%] h-5 w-5 translate-x-[-50%] translate-y-[-50%] rounded-full bg-blue-300" />
-                <div className="absolute left-[50%] top-[50%] z-[-1] flex h-[100%] w-1 translate-x-[-50%] flex-col justify-between">
+                <div className="absolute left-[0] top-[50%] hidden h-5 w-5 translate-x-[-50%] translate-y-[-50%] rounded-full bg-blue-300 md:left-[50%] md:block" />
+                <div
+                  className={cn(
+                    "absolute left-[50%] top-[50%] z-[-1] hidden h-[100%] w-1 translate-x-[-50%] flex-col justify-between md:left-[50%] md:flex",
+                  )}
+                >
                   {[...Array(3)].map((_, i) => {
                     return (
                       <div
@@ -61,7 +65,7 @@ const JourneyPageContent: React.FC<Props> = ({ journeyDict }) => {
             );
           })}
           <li className="relative grid h-full grid-rows-[1fr_1fr] justify-center">
-            <Button containerClassName="row-start-2 row-end-3 mt-8">
+            <Button containerClassName="md:row-start-2 md:row-end-3 mt-8">
               View My Projects
             </Button>
           </li>

c47be1c4279d7675689bbe8add676de04778acc6
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-08-29 18:16:06 +0400

    fixed journey path animations

--- app/components/JourneyPageContent.tsx
+++ app/components/JourneyPageContent.tsx
@@ -1,13 +1,12 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import OutlinedText from "@/app/components/OutlineText";
 import { cn, getColorRGBA } from "@/app/utils/tailwind";
 import Button from "@/app/components/Button";
 import { generateTranslator } from "../utils/i18n";
 import { DictionarySection } from "../[lang]/dictionaries";
-import { motion, useAnimate } from "framer-motion";
+import { Segment, motion, useAnimate } from "framer-motion";
 import LineWaves from "./LineWaves";
-import { useInView } from "react-intersection-observer";
 
 const journeys = [
   { id: "journey_start", date: "2017" },
@@ -15,33 +14,91 @@ const journeys = [
   { id: "journey_profession_intro", date: "2021" },
   { id: "journey_start", date: "2017" },
   { id: "journey_web_focus", date: "2019" },
-  { id: "journey_profession_intro", date: "2021" },
-  { id: "journey_start", date: "2017" },
-  { id: "journey_web_focus", date: "2019" },
-  { id: "journey_profession_intro", date: "2021" },
-  { id: "journey_start", date: "2017" },
-  { id: "journey_web_focus", date: "2019" },
-  { id: "journey_profession_intro", date: "2021" },
 ] as const;
 
 type Props = {
   journeyDict: DictionarySection<"journey">;
 };
 
+class FuncQueue {
+  queue: Function[];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  async enqueue(f: Function) {
+    this.queue.push(f);
+    if (this.isOnlyHead()) {
+      this.runHead();
+    }
+  }
+
+  async runHead() {
+    if (this.isEmpty()) return;
+    await this.queue[0]();
+    this.queue.shift();
+    this.runHead();
+  }
+
+  isOnlyHead() {
+    return this.queue.length === 1;
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+}
+
 const JourneyPageContent: React.FC<Props> = ({ journeyDict }) => {
   const t = generateTranslator<"journey">(journeyDict);
-  const itemRefs = useRef<HTMLLIElement[]>([]);
   const [scope, animate] = useAnimate();
-  const [visibleAmount, setVisibleAmount] = useState(0);
+  const funcQueue = useMemo(() => new FuncQueue(), []);
+  const [animatedElements, setAnimatedElements] = useState<string[]>([]);
 
-  useEffect(() => {
-      const tempItems = itemRefs.current.slice(0, visibleAmount)
-      tempItems.forEach(el => {
-        animate(el, {opacity: .5}, {duration: 1, delay: 1})
-          })
+  const viewportEnterEnqueue = (
+    entry: IntersectionObserverEntry | null,
+    animationFunc: Function,
+  ) => {
+    if (!entry) return;
+    const el = entry.target;
+    if (animatedElements.includes(el.id)) return;
+    setAnimatedElements((els) => [...els, el.id]);
 
-    console.log(visibleAmount)
-  }, [scope, animate, itemRefs, visibleAmount]);
+    funcQueue.enqueue(() => animationFunc(el));
+  };
+
+  const generateItemAnimationFunc = () => {
+    return (el: Element) => {
+      const pathAnims: Segment[] = [...Array(3)].map((_, j) => [
+        `#${el.id}-path-${j}`,
+        { opacity: 1, scaleY: "100%" },
+        {
+          duration: 0.25 / 3,
+        },
+      ]);
+      return animate([
+        [
+          `#${el.id}-dot`,
+          {
+            opacity: 1,
+            transform: "scale(1) translateX(-50%) translateY(-50%)",
+          },
+          {
+            duration: 0.25,
+          },
+        ],
+        [
+          `#${el.id}-card`,
+          { opacity: 1, transform: "translateX(0)" },
+          {
+            duration: 0.25,
+          },
+        ],
+        ...pathAnims,
+      ]);
+    };
+  };
 
   return (
     <>
@@ -59,71 +116,51 @@ const JourneyPageContent: React.FC<Props> = ({ journeyDict }) => {
             const isOdd = i % 2 !== 0;
             return (
               <motion.li
+                id={`journey-card-${i}`}
                 key={journey.id}
                 className="relative z-[-1] flex h-full flex-col justify-center md:py-2"
-                ref={(el) => el && (itemRefs.current[i] = el)}
-                    onViewportEnter={() => {
-                      setVisibleAmount((currAmount) =>
-                        Math.max(currAmount, i + 1),
-                      );
-                    }}
+                ref={(el) => el && (el = el)}
+                onViewportEnter={(entry) => {
+                  viewportEnterEnqueue(entry, generateItemAnimationFunc());
+                }}
               >
-                <motion.div
+                <motion.section
+                  id={`journey-card-${i}-card`}
+                  className={cn(
+                    "my-auto flex h-fit w-full flex-col rounded-xl bg-gradient-to-r px-6 py-4 backdrop-blur-md md:w-[46%] md:backdrop-blur-lg",
+                    {
+                      "md:ml-auto": isOdd,
+                    },
+                  )}
+                  style={{
+                    background: `linear-gradient(${
+                      isOdd ? "to left" : "to right"
+                    },${getColorRGBA("neutral-900", 0.3)},${getColorRGBA(
+                      "neutral-700",
+                      0.3,
+                    )} )`,
+                  }}
                   initial={{
                     opacity: 0,
-
                     translateX: (isOdd ? "-" : "") + "16px",
                   }}
-                  animate={{
-                    opacity: 1,
-                    translateX: "0",
-                  }}
-                  transition={{ delay: 0.25, duration: 0.25 }}
                 >
-                  <motion.section
-                    className={cn(
-                      "my-auto flex h-fit w-full flex-col rounded-xl bg-gradient-to-r px-6 py-4 backdrop-blur-md md:w-[46%] md:backdrop-blur-lg",
-                      {
-                        "md:ml-auto": isOdd,
-                      },
-                    )}
-                    style={{
-                      background: `linear-gradient(${
-                        isOdd ? "to left" : "to right"
-                      },${getColorRGBA("neutral-900", 0.3)},${getColorRGBA(
-                        "neutral-700",
-                        0.3,
-                      )} )`,
-                    }}
-                    initial={{
-                      opacity: 0,
-                      translateX: (isOdd ? "-" : "") + "16px",
-                    }}
-                    viewport={{ once: true }}
-                    whileInView={{ opacity: 1, translateX: "0" }}
-                    transition={{ duration: 0.25 }}
-                  >
-                    <div className="mb-2 flex items-center justify-between gap-2">
-                      <h3 className="text-clamp-xl">{journeyCardDict.title}</h3>
-                      <span className="text-clamp-sm">{journey.date}</span>
-                    </div>
-                    <p className="text-clamp-rg">{journeyCardDict.body}</p>
-                  </motion.section>
-                </motion.div>
-                <motion.div>
+                  <div className="mb-2 flex items-center justify-between gap-2">
+                    <h3 className="text-clamp-xl">{journeyCardDict.title}</h3>
+                    <span className="text-clamp-sm">{journey.date}</span>
+                  </div>
+                  <p className="text-clamp-rg">{journeyCardDict.body}</p>
+                </motion.section>
+                <div>
                   <motion.div
                     initial={{
                       opacity: 0,
                       transform: "scale(0) translateX(-50%) translateY(-50%)",
                     }}
-                    animate={{
-                      opacity: 1,
-                      transform: "scale(1) translateX(-50%) translateY(-50%)",
-                    }}
-                    transition={{ duration: 0.25 }}
+                    id={`journey-card-${i}-dot`}
                     className="absolute left-[50%] top-[50%] hidden h-5 w-5 translate-x-[-50%] translate-y-[-50%] rounded-full bg-blue-300 [transform-origin:0_0] md:block"
                   />
-                  <motion.div
+                  <div
                     className={cn(
                       "absolute left-[50%] top-[50%] z-[-1] hidden h-[100%] w-1 translate-x-[-50%] flex-col justify-between md:left-[50%] md:flex",
                     )}
@@ -131,28 +168,27 @@ const JourneyPageContent: React.FC<Props> = ({ journeyDict }) => {
                     {[...Array(3)].map((_, j) => {
                       return (
                         <motion.div
+                          id={`journey-card-${i}-path-${j}`}
                           key={j}
                           className="h-[24%] w-full rounded-full bg-slate-600 [transform-origin:-100%_0]"
                           initial={{ opacity: 0, scaleY: "0%" }}
-                          whileInView={{ opacity: 1, scaleY: "100%" }}
-                          transition={{
-                            delay: (0.25 / 3) * j,
-                            duration: 0.25 / 3,
-                          }}
                         />
                       );
                     })}
-                  </motion.div>
-                </motion.div>
+                  </div>
+                </div>
               </motion.li>
             );
           })}
           <div className="relative grid h-full justify-center md:grid-rows-[1fr_1fr]">
             <motion.div
               initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
               className="mt-8 md:row-start-2 md:row-end-3"
+              onViewportEnter={(entry) => {
+                viewportEnterEnqueue(entry, (el: Element) =>
+                  animate(el, { opacity: 1 }, { duration: 1 }),
+                );
+              }}
             >
               <Button className="sm:clamp-xl text-[16px]">
                 View My Projects

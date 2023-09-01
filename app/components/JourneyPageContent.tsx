0ed22e40348174aedec286c91fc9a37eb512636f
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-01 23:34:53 +0400

    restoring journey page div styles

--- app/components/JourneyPageContent.tsx
+++ app/components/JourneyPageContent.tsx
@@ -31,37 +31,39 @@ const JourneyPageContent: React.FC<Props> = ({ journeyDict }) => {
 
   return (
     <>
-      <main className="w-full md:w-clamp-card-container" ref={scope}>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.25 }}
-        >
-          <OutlinedText>Journey</OutlinedText>
-        </motion.div>
-        <div className="hidden w-full justify-center md:flex">
-          <JourneyDesktopList
-            journeyDict={journeyDict}
-            viewportEnterEnqueue={viewportEnterEnqueue}
-            animate={animate}
-          />
-        </div>
-        <div className="flex w-full justify-center md:hidden">
-          <JourneyMobileList
-            journeyDict={journeyDict}
-            viewportEnterEnqueue={viewportEnterEnqueue}
-            animate={animate}
-          />
-        </div>
-      </main>
-      <LineWaves
-        type="journeyLeft"
-        className="bottom-0 left-0 max-h-[200%] w-[140%] translate-x-[-61%] translate-y-[24%]"
-      />
-      <LineWaves
-        type="journeyRight"
-        className="bottom-0 right-0 max-h-[100%] w-[69%] translate-x-[34%] translate-y-[10%]"
-      />
+      <div className="flex flex-col items-center p-8">
+        <main className="w-full md:w-clamp-card-container" ref={scope}>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.25 }}
+          >
+            <OutlinedText>Journey</OutlinedText>
+          </motion.div>
+          <div className="hidden w-full justify-center md:flex">
+            <JourneyDesktopList
+              journeyDict={journeyDict}
+              viewportEnterEnqueue={viewportEnterEnqueue}
+              animate={animate}
+            />
+          </div>
+          <div className="flex w-full justify-center md:hidden">
+            <JourneyMobileList
+              journeyDict={journeyDict}
+              viewportEnterEnqueue={viewportEnterEnqueue}
+              animate={animate}
+            />
+          </div>
+        </main>
+        <LineWaves
+          type="journeyLeft"
+          className="bottom-0 left-0 max-h-[200%] w-[140%] translate-x-[-61%] translate-y-[24%]"
+        />
+        <LineWaves
+          type="journeyRight"
+          className="bottom-0 right-0 max-h-[100%] w-[69%] translate-x-[34%] translate-y-[10%]"
+        />
+      </div>
     </>
   );
 };

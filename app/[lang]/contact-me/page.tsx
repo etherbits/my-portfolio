118a5d377cb3c1589c008d8f65d468cb7c961004
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-03 23:54:40 +0400

    adding contact me page

---
+++ app/[lang]/contact-me/page.tsx
@@ -0,0 +1,14 @@
+import { getDictionary } from "../dictionaries";
+import { Locale } from "@/middleware";
+import ProjectsPageContent from "@/app/components/ProjectsPageContent";
+
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dict = await getDictionary(lang);
+  return (
+  <div>contact me</div>
+  );
+}

24a31db257caf89346a769a9050151ab366cc86e
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-04 22:40:25 +0400

    adjusting contact me page styling

--- app/[lang]/contact-me/page.tsx
+++ app/[lang]/contact-me/page.tsx
@@ -2,6 +2,7 @@ import { getDictionary } from "../dictionaries";
 import { Locale } from "@/middleware";
 import ProjectsPageContent from "@/app/components/ProjectsPageContent";
 import Input from "@/app/components/Input";
+import OutlinedText from "@/app/components/OutlineText";
 
 export default async function Home({
   params: { lang },
@@ -10,7 +11,8 @@ export default async function Home({
 }) {
   const dict = await getDictionary(lang);
   return (
-    <div className="flex flex-col items-center p-8">
+    <div className="flex flex-col items-center gap-8 px-8 py-4">
+      <OutlinedText>Contact Me</OutlinedText>
       <form className="flex flex-col gap-4">
         <Input
           icon="User"

dd687519f0e93d5366cd420197ced68cb7eec8e3
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-07 00:27:09 +0400

    switching back to resend for emails

--- app/lib/email.ts
+++ app/lib/email.ts
@@ -1,13 +1,14 @@
 "use server";
-import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
+import {Resend} from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendMail() {
   const res = await resend.emails.send({
-    from: "test@gmail.com",
+    from: "onboarding@resend.dev",
     to: "nika.qvrivishvilipc@gmail.com",
     subject: "Hello from resend",
-    html: "<p>Congrats on sending your <strong>first resend email</strong>!</p>",
+    html: "<p>Congrats on sending your <strong>first nodemailer email</strong>!</p>",
   });
 
   console.log(res);
